7a1f6bf904e24b21284ed8dd159e9c9f16f9e0f3
Igor Gajewski <Igor.Gajewski@sdimedia.com>
2019-05-21 13:06:11 +0200

    radio buttons for encryption/decryption mode

--- src/SecretSharing.java
+++ src/SecretSharing.java
@@ -2,101 +2,135 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
 import java.io.File;
 import java.io.IOException;
 
 class MainFrame extends JFrame{
-     MainFrame(){
-        super("Secret Sharing");
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setVisible(true);
-        setLayout(new GridBagLayout());
-        GridBagConstraints gbc = new GridBagConstraints();
-        setSize(new Dimension(360, 640));
-        setResizable(false);
+     MainFrame() {
+         super("Secret Sharing");
+         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+         setVisible(true);
+         setLayout(new GridBagLayout());
+         GridBagConstraints gbc = new GridBagConstraints();
+         setSize(new Dimension(360, 640));
+         setResizable(false);
 
-        // row 0
-        gbc.gridy=0;
-        JLabel head = new JLabel("Secret Sharing!");
-        gbc.gridx=0;
-        gbc.gridwidth=4;
-        add(head, gbc);
+         // row 0 - TITLE
+         gbc.gridy = 0;
+         JLabel head = new JLabel("Secret Sharing!");
+         gbc.gridx = 0;
+         gbc.gridwidth = 4;
+         add(head, gbc);
 
-        // row 1
-         gbc.gridy=1;
-         gbc.gridx=0;
-         gbc.gridwidth=1;
-
+         // row 1 - SELECT FILE LABEL
+         gbc.gridy = 1;
+         gbc.gridx = 0;
+         gbc.gridwidth = 1;
          add(new JLabel("Select file: "), gbc);
-        // row 2
-        gbc.gridy=2;
-        gbc.gridx=0;
-        gbc.gridwidth=3;
-        JTextField path = new JTextField();
-        path.setPreferredSize(new Dimension(260, 25));
-        add(path, gbc);
-        JButton browse = new JButton("...");
-        JFileChooser fc = new JFileChooser();
-        browse.addActionListener((ActionEvent browseAction) -> {
-            int returnVal = fc.showOpenDialog(browse);
-            if (returnVal == JFileChooser.APPROVE_OPTION) {
-                final File f = fc.getSelectedFile();
-                path.setText(f.getPath());
-            }
-        });
-        browse.setPreferredSize(new Dimension(25,25));
-        gbc.gridx=3;
-        gbc.gridwidth=1;
-        add(browse,gbc);
 
-        // row 3
-         gbc.gridy=3;
-         gbc.gridx=0;
+         // row 2 - FILE CHOOSER
+         gbc.gridy = 2;
+         gbc.gridx = 0;
+         gbc.gridwidth = 3;
+         JTextField path = new JTextField();
+         path.setPreferredSize(new Dimension(260, 25));
+         add(path, gbc);
+         JButton browse = new JButton("...");
+         JFileChooser fc = new JFileChooser();
+         browse.addActionListener((ActionEvent browseAction) -> {
+             int returnVal = fc.showOpenDialog(browse);
+             if (returnVal == JFileChooser.APPROVE_OPTION) {
+                 final File f = fc.getSelectedFile();
+                 path.setText(f.getPath());
+             }
+         });
+         browse.setPreferredSize(new Dimension(25, 25));
+         gbc.gridx = 3;
+         gbc.gridwidth = 1;
+         add(browse, gbc);
+
+         // row 3 EMPTY
+         // row 4 - ENCRYPT/DECRYPT RADIO
+         gbc.gridy = 4;
+         gbc.gridx = 0;
+
+         JRadioButton encrypt = new JRadioButton("Encrypt");
+         //encrypt.addActionListener(this);
+         add(encrypt, gbc);
+
+         gbc.gridx = 2;
+         JRadioButton decrypt = new JRadioButton("Decrypt");
+         add(decrypt, gbc);
+
+         ButtonGroup operations = new ButtonGroup();
+         operations.add(encrypt);
+         operations.add(decrypt);
+
+         // row 5
+         gbc.gridy = 5;
+         gbc.gridx = 0;
          add(new JLabel("k:"), gbc);
-         gbc.gridx=1;
+         gbc.gridx = 1;
          JTextField k = new JTextField();
-         k.setPreferredSize(new Dimension(25,25));
+         k.setPreferredSize(new Dimension(25, 25));
          add(k, gbc);
-         gbc.gridx=2;
+         gbc.gridx = 2;
          add(new JLabel("n:"), gbc);
-         gbc.gridx=3;
+         gbc.gridx = 3;
          JTextField n = new JTextField();
-         n.setPreferredSize(new Dimension(25,25));
+         n.setPreferredSize(new Dimension(25, 25));
          add(n, gbc);
-        // row 4
-         gbc.gridy=4;
-         gbc.gridx=0;
+
+         // row 6
+         gbc.gridy = 6;
+         gbc.gridx = 0;
          add(new JLabel("r:"), gbc);
-         gbc.gridx=1;
+         gbc.gridx = 1;
          JTextField r = new JTextField();
-         r.setPreferredSize(new Dimension(25,25));
+         r.setPreferredSize(new Dimension(25, 25));
          add(r, gbc);
 
-         gbc.gridx=2;
+         gbc.gridx = 2;
          add(new JLabel("m:"), gbc);
-         gbc.gridx=3;
+         gbc.gridx = 3;
          JTextField m = new JTextField();
-         m.setPreferredSize(new Dimension(25,25));
+         m.setPreferredSize(new Dimension(25, 25));
          add(m, gbc);
 
          JTextArea test = new JTextArea("TEST");
          test.setEditable(false);
-         // row 5
-         gbc.gridy=5;
-         gbc.gridwidth=2;
-         gbc.gridheight=1;
+
+         // row 7
+         gbc.gridy = 7;
+         gbc.gridwidth = 2;
+         gbc.gridheight = 1;
          JButton encode = new JButton("Encode!");
-         gbc.gridx=0;
+         encode.setMnemonic(KeyEvent.VK_ENTER);
+         gbc.gridx = 0;
          add(encode, gbc);
-         encode.addActionListener((ActionEvent encodeAction) -> test.setText("path: "+path.getText()+"\r\nk: "+k.getText()+", n: "+n.getText()+", r: "+r.getText()+", m: "+m.getText()));
+
+
+
+         encode.addActionListener((ActionEvent encodeAction) -> {
+             String mode;
+             if (encrypt.isSelected())
+                 mode = "encrypt";
+             else if (decrypt.isSelected())
+                 mode = "decrypt";
+             else
+                 mode = "ERROR";
+                 test.setText("mode: " + mode + "\npath: " + path.getText() + "\nk: " + k.getText() + ", n: " + n.getText() + ", r: " + r.getText() + ", m: " + m.getText());
+
+         });
 
          JButton exit = new JButton("Exit!");
          gbc.gridx=2;
          add(exit, gbc);
          exit.addActionListener((ActionEvent exitAction) -> System.exit(0));
 
-         // row 6
-         gbc.gridy=6;
+         // row 8
+         gbc.gridy=8;
          gbc.gridx=0;
          gbc.gridwidth=4;
          gbc.gridheight=3;
