74daaee04fe6d3329bcac7ec44d3688918bd2683
Igor Gajewski <igorgajewski94@gmail.com>
2019-05-22 19:17:34 +0200

    encode/decode functionality

--- src/SecretSharing.java
+++ src/SecretSharing.java
@@ -1,17 +1,31 @@
 import javax.swing.*;
+import javax.swing.filechooser.FileNameExtensionFilter;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.image.BufferedImage;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.util.*;
 
 class MainFrame extends JFrame{
     private File f;
     private String mode;
     private String fullPath;
     private String directoryPath;
+    private String fileBasename;
+    private int k, n, m, r, a; // k,n from (k,n)threshold scheme, r - neighborhood radius, m, a - algorithm parameters
+    private Random rng = new Random();
+    private String encoded;
+    private FileWriter fw;
+    private Scanner s;
+    private String finalConfiguration;
+    private boolean result;
+    private boolean ruleNumbersFlag = false;
+    private int[] ruleNumbers;
      MainFrame() {
          super("Secret Sharing");
          setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -43,6 +57,8 @@ class MainFrame extends JFrame{
          add(path, gbc);
          JButton browse = new JButton("...");
          JFileChooser fc = new JFileChooser();
+         FileNameExtensionFilter filter = new FileNameExtensionFilter("*.txt", "txt");
+         fc.setFileFilter(filter);
 
          browse.addActionListener((ActionEvent browseAction) -> {
              int returnVal = fc.showOpenDialog(browse);
@@ -50,6 +66,7 @@ class MainFrame extends JFrame{
                  f = fc.getSelectedFile();
                  fullPath = f.getPath();
                  directoryPath = f.getParent();
+                 fileBasename = f.getName().replaceFirst("[.][^.]+$", "");
                  path.setText(fullPath);
              }
          });
@@ -84,35 +101,36 @@ class MainFrame extends JFrame{
          gbc.gridx = 0;
          add(new JLabel("k:"), gbc);
          gbc.gridx = 1;
-         JTextField k = new JTextField();
-         k.setPreferredSize(new Dimension(25, 25));
-         add(k, gbc);
+         JTextField kField = new JTextField();
+         kField.setPreferredSize(new Dimension(25, 25));
+         add(kField, gbc);
          gbc.gridx = 2;
          add(new JLabel("n:"), gbc);
          gbc.gridx = 3;
-         JTextField n = new JTextField();
-         n.setPreferredSize(new Dimension(25, 25));
-         add(n, gbc);
+         JTextField nField = new JTextField();
+         nField.setPreferredSize(new Dimension(25, 25));
+         add(nField, gbc);
 
          // row 6
          gbc.gridy = 6;
          gbc.gridx = 0;
          add(new JLabel("r:"), gbc);
          gbc.gridx = 1;
-         JTextField r = new JTextField();
-         r.setPreferredSize(new Dimension(25, 25));
-         add(r, gbc);
+         JTextField rField = new JTextField();
+         rField.setPreferredSize(new Dimension(25, 25));
+         add(rField, gbc);
 
          gbc.gridx = 2;
          add(new JLabel("m:"), gbc);
          gbc.gridx = 3;
-         JTextField m = new JTextField();
-         m.setPreferredSize(new Dimension(25, 25));
-         add(m, gbc);
+         JTextField mField = new JTextField();
+         mField.setPreferredSize(new Dimension(25, 25));
+         add(mField, gbc);
          JTextArea test = new JTextArea();
          test.setOpaque(false);
          test.setBackground(new Color(0,0,0,0));
          test.setEditable(false);
+         test.setFont(new Font(Font.MONOSPACED, Font.BOLD, 12));
 
          // row 7 - BUTTONS
          gbc.gridy = 7;
@@ -124,14 +142,98 @@ class MainFrame extends JFrame{
          add(encode, gbc);
 
          encode.addActionListener((ActionEvent encodeAction) -> {
+             k = Integer.parseInt(kField.getText());
+             n = Integer.parseInt(nField.getText());
+             m = Integer.parseInt(mField.getText());
+             r = Integer.parseInt(rField.getText());
              if (encrypt.isSelected())
                  mode = "encrypt";
              else if (decrypt.isSelected())
                  mode = "decrypt";
              else
                  mode = "ERROR";
+             try {
+                 encoded = encode(fullPath);
+                 test.append("mode: " + mode + "\nfile: " + fullPath + "\nk: " + k + ", n: " + n + ", r: " + r + ", m: " + m);
+             }
+             catch (Exception e) {
+                test.append("\nfile not found: " + fullPath);
+             }
+             if(!ruleNumbersFlag) {
+                 ruleNumbers = new int[k - 1];
+                 for (int i = 0; i < k - 1; i++) {
+                     int o = 0;
+                     while (o == 0) {
+                         o = rng.nextInt((int) Math.pow(2.0, (double) (2 * r + 1)));
+                     }
+                     ruleNumbers[i] = o;
+                 }
+                 ruleNumbersFlag = true;
+             }
+             test.append("\nrule numbers:" + Arrays.toString(ruleNumbers));
+             if(mode.equals("encrypt")){
+                 ArrayList<String> generatedConfigurations = new ArrayList<>();
+                 generatedConfigurations.add(encoded);
+                 for(int i = 0; i < k - 1; i++){
+                     StringBuilder sb = new StringBuilder();
+                     for(int j = 0; j < encoded.length(); j++){
+                         int a = rng.nextInt(2);
+                         sb.append(a);
+                     }
+                     generatedConfigurations.add(new String(sb));
+                 }
+                 for (int i = 0; i < m + n - k; i++){
+                     generatedConfigurations.add(nextSequence(r, ruleNumbers, new String[]{(String)generatedConfigurations.toArray()[i], (String)generatedConfigurations.toArray()[i+1], (String)generatedConfigurations.toArray()[i+2]}, generatedConfigurations.get(0).length()));
+                 }
+                 f = new File(directoryPath + "/" + fileBasename + "_shares.txt");
+                 try {
+                     result = f.exists() ? f.delete() : f.createNewFile();
+                     fw = new FileWriter(f, false);
+                     for (int i = generatedConfigurations.size() - n; i < generatedConfigurations.size(); i++) {
+                         fw.write(generatedConfigurations.get(i) + "\r\n");
+                     }
+                     fw.close();
+                     test.append("\nshares to be distributed have been saved to shares.txt file");
+                 }
+                 catch (Exception e){
+                     test.append("\nunable to create _shares file from " + fileBasename);
+                 }
+             }
+             else if (mode.equals("decrypt")){
+                 a = rng.nextInt(n - k + 1);
+                 ArrayList<String> inverseConfigurations = new ArrayList<>();
+                 // take configurations from shares.txt file
+                 try {
+                     s = new Scanner(new File(fullPath));
+                     for(int i = 0; i < k; i++)
+                         inverseConfigurations.add(s.nextLine());
+                 }
+                 catch(Exception e){
+                     test.append("\nfile not found: " + fullPath);
+                 }
+                 Collections.reverse(inverseConfigurations);
 
-             test.setText("mode: " + mode + "\npath: " + fullPath + "\ndirectory path: " + directoryPath + "\nk: " + k.getText() + ", n: " + n.getText() + ", r: " + r.getText() + ", m: " + m.getText());
+                 int[] ruleNumbersInversed = ruleNumbers.clone();
+                 reverseArray(ruleNumbersInversed);
+                 for(int i = 0; i < m + a + k - 3; i++){
+                     inverseConfigurations.add(nextSequence(r, ruleNumbersInversed, new String[]{(String)inverseConfigurations.toArray()[i], (String)inverseConfigurations.toArray()[i+1], (String)inverseConfigurations.toArray()[i + 2]}, inverseConfigurations.get(0).length()));
+                 }
+                 //decode
+                 finalConfiguration = inverseConfigurations.get(inverseConfigurations.size()-1);
+                 f = new File( directoryPath + "/" + fileBasename + "_secret.txt");
+                 try {
+                     result = f.exists() ? f.delete() : f.createNewFile();
+                     fw = new FileWriter(f, false);
+                     fw.write(decode(finalConfiguration));
+                     fw.close();
+                 }
+                 catch(Exception e){
+                     test.append("\nunable to create _secret file from " + fileBasename);
+                 }
+             }
+
+
+             test.append("\ncompleted!\n");
 
          });
 
@@ -152,8 +254,87 @@ class MainFrame extends JFrame{
          gbc.gridwidth=4;
          gbc.gridheight=3;
 
-         test.setPreferredSize(new Dimension(260,260));
+         test.setPreferredSize(new Dimension(300,260));
          add(test, gbc);
+    }
+    private static String encode(String filePath) throws FileNotFoundException {
+        StringBuilder binString = new StringBuilder();
+        try (Scanner file = new Scanner(new File(filePath))) {
+            while (file.hasNextLine()) {
+                String nextLine = file.nextLine();
+                char[] charArray = nextLine.toCharArray();
+                for (char c : charArray) {
+                    String s = Integer.toBinaryString(((int) c));
+                    StringBuilder sb = new StringBuilder(s);
+                    if (s.length() < 8) {
+                        for (int i = 0; i < 8 - s.length(); i++)
+                            sb.insert(0, "0");
+                    }
+                    binString.append(new String(sb));
+                }
+            }
+        }
+        return new String(binString);
+    }
+    private static String decode (String content){
+        StringBuilder sb = new StringBuilder();
+        int startIndex = 0, endIndex = 8;
+        ArrayList<String> stringArrayList = new ArrayList<>();
+        while(endIndex != content.length() + 8) {
+            stringArrayList.add(content.substring(startIndex, endIndex));
+            startIndex = endIndex;
+            endIndex += 8;
+        }
+        for(String s : stringArrayList){
+            sb.append((char)Integer.parseInt(s, 2));
+        }
+        return new String(sb);
+    }
+
+    private static int[] dec2bin(int dec, int r){
+        int[] binArray = new int[r+2];
+        int counter = 0;
+        while(dec > 0){
+            int a = dec % 2;
+            binArray[r + 1 - counter] = a;
+            counter++;
+            dec /=2 ;
+        }
+        return binArray;
+    }
+
+    private static int nextElement(String[] sequences, int length, int[][] binaryRuleNumbers, int i){
+        int Element = 0;
+        int counter = 0;
+        for(int j = sequences.length - 1; j > 0; j--){
+            for(int k = 0; k < binaryRuleNumbers[j - 1].length; k++){
+                Element += binaryRuleNumbers[counter][k] * (sequences[j].toCharArray()[(i+k-1+length)%length])%2;
+            }
+            counter++;
+        }
+        Element += sequences[0].toCharArray()[i];
+        return Element%2;
+    }
+
+    private static String nextSequence(int r, int[] ruleNumbers, String[] sequences, int length){
+        StringBuilder sb = new StringBuilder();
+        int[][] binaryRuleNumbers = new int[ruleNumbers.length][r+2];
+        for(int i = 0; i < ruleNumbers.length; i++){
+            binaryRuleNumbers[i] = dec2bin(ruleNumbers[i], r);
+        }
+        for(int i = 0; i < length; i++){
+            sb.append(nextElement(sequences, length, binaryRuleNumbers, i));
+        }
+        return new String(sb);
+    }
+
+    private static void reverseArray(int[] arr){
+        for(int i = 0; i < arr.length / 2; i++)
+        {
+            int temp = arr[i];
+            arr[i] = arr[arr.length - i - 1];
+            arr[arr.length - i - 1] = temp;
+        }
     }
 }
 
